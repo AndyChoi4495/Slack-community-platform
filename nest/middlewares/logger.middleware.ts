0ad345d9209356d798957d02b7557dc2054c1842
AndyChoi <AndyChoi4495@github.com>
2023-10-11 16:00:39 -0400

    first commit

---
+++ nest/middlewares/logger.middleware.ts
@@ -0,0 +1,7 @@
+import{Injectable, Logger, NestMiddleware } from '@nestjs/common';
+
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+    private logger = new Logger('HTTP')
+}
---
+++ nest/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { ChannelsModule } from './channels/channels.module';
+import { DmsModule } from './dms/dms.module';
+
+@Module({
+  imports: [ConfigModule.forRoot(), UsersModule, WorkspacesModule, ChannelsModule, DmsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
